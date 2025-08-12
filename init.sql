eb4efcbf3e29251a28565b44eadd8824a94905a2
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-08-12 15:05:07 -0500

    Comment SQL init

--- init.sql
+++ init.sql
@@ -1,2 +1,4 @@
+-- Setup users and database.
+-- PostgresML does not use environment variables like the standard Postgres image does.
 CREATE USER adaptionuser WITH PASSWORD 'insecurepassword';
 CREATE DATABASE cats OWNER adaptionuser;
